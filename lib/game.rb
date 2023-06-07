18fafede882bb08c4f0880bf8ad480888fc568f4
MKMaths22 <120586172+MKMaths22@users.noreply.github.com>
2023-06-07 15:30:52 +0100

    Create lib, spec files and dummy command for executing play_connect_four

---
+++ lib/game.rb
@@ -0,0 +1,10 @@
+# The Game class takes care of initializing other classes and whose turn it is next
+class Game
+  def initialize
+    @this_game = play_game
+  end
+
+  def play_game
+    puts 'This is dummy text, dummy!'
+  end
+end
---
+++ lib/play_connect_four.rb
@@ -0,0 +1,5 @@
+# This file houses the top-level code that executes the game, allowing RSpec 
+# to do testing on game.rb, which may be later split up according to classes
+
+require './game.rb'
+Game.new
