1d10a5a104f9d31904cb591b0942ef19e20a1ad8
MKMaths22 <120586172+MKMaths22@users.noreply.github.com>
2023-06-09 15:00:00 +0100

    Asking for help here

--- lib/game.rb
+++ lib/game.rb
@@ -12,7 +12,10 @@ class Game
   end
 
   def play_game
+    puts "Welcome to Connect Four!"
+    sleep(2)
     create_players
+    ask_second_disk
   end
 
   def create_players
@@ -21,13 +24,36 @@ class Game
   end
 
   def ask_first_name
-  
+    puts "Player One, please enter your name:"
+    gets.strip
   end
 
   def ask_second_name
-  
+    puts "Player Two, please enter your name:"
+    gets.strip
   end
 
+  def ask_second_disc
+    if disc_input == 'Y'
+      @player_one.set_red
+      @player_two.set_yellow
+    else 
+      @player_one.set_yellow
+      @player_two.set_red
+    end
+  end
+
+  def disc_input
+    puts "Player Two, you get to choose colours."
+    sleep(2)
+    puts "Type 'R' for red discs, or 'Y' for yellow."
+    valid_input(['R', 'Y'])
+  end
+
+  def valid_input(array)
+
+  end
+  
   def one_turn
     @current_player = @current_player == @player_one ? player_two : player_one
   end
@@ -37,8 +63,20 @@ class Game
 end
 
 class Player
+  
+  attr_accessor :name, :disc
+  
   def initialize(name)
     @name = name
+    @disc = nil
+  end
+
+  def set_red
+    @disc = 'Red'
+  end
+
+  def set_yellow
+    @disc = 'Yellow'
   end
 end
 
--- spec/connect_four-spec.rb
+++ spec/connect_four-spec.rb
@@ -3,6 +3,17 @@ require '../lib/game'
 
   describe Game do
     subject(:game) { described_class.new }
+      
+    
+      describe "#ask_first_name" do
+        it "asks the first player their name" do
+          expect(game).to receive(:puts).with('Player One, please enter your name:')
+          game.ask_first_name
+        end
+    
+      end
+      
+      
       describe "#create_players" do
         context 'When Peter and Chris play' do
           before do 
@@ -20,8 +31,6 @@ require '../lib/game'
       describe "#one_turn" do
         context 'player_one has had a turn, so player_two should be next' do
           before do
-            # let(:Peter) { double(Player) }
-            # let(:Chris) { double(Player) }
             # current_player = game.instance_variable_get(:@current_player)
             # peter = game.instance_variable_get(:@player_one)
             # current_player = peter
@@ -38,6 +47,31 @@ require '../lib/game'
           end
         end
       end
+
+      describe "#ask_second_disc" do
+        context 'Second player chooses yellow' do
+            let(:chris) { instance_double(Player, 'Chris') }
+            let(:peter) { instance_double(Player, 'Peter') }
+            it 'sends the second player message to set disc to yellow' do
+              allow(game).to receive(:disc_input).and_return('Y')
+              allow(game).to receive(:@player_two).and_return(chris)
+              expect(chris).to receive(:set_yellow)
+              game.ask_second_disc
+            end
+
+            it 'sends the first player message to set disc to red' do
+              allow(game).to receive(:disc_input).and_return('Y')
+              allow(game).to receive(:@player_one).and_return(peter)
+              expect(peter).to receive(:set_red)
+              game.ask_second_disc
+            end
+
+
+        end
+
+      end
+
+
 
 
       
