b2c724378d35d57a2521d2b594321daee330e6db
MKMaths22 <120586172+MKMaths22@users.noreply.github.com>
2023-06-16 13:11:40 +0100

    Use Rucocop to autocorrect board.rb

--- lib/board.rb
+++ lib/board.rb
@@ -5,54 +5,59 @@ require 'pry-byebug'
 
 # Board class keeps score of where the discs are and game ending conditions
 class Board
-
   attr_accessor :cells_array
 
-  def initialize(cells_array = Array.new(7) { Array.new(6) } )
+  def initialize(cells_array = Array.new(7) { Array.new(6) })
     @cells_array = cells_array
   end
 
   def try_adding_tile(column, disc)
-    # The column is inputted as a STRING numbered from 1 to 7, which will have to be reinterpreted 
+    # The column is inputted as a STRING numbered from 1 to 7, which will have to be reinterpreted
     actual_column = column.to_i - 1
     return 'full' if cells_array[actual_column][5]
-  
+
     cell_to_use = cells_array[actual_column].index(nil)
     cells_array[actual_column][cell_to_use] = disc
     return 'game_won' if check_if_game_won?(cells_array, actual_column, cell_to_use)
 
     'game_drawn' if check_if_game_drawn?(cells_array, cell_to_use)
-
   end
 
   def top_half(char)
     seven_spaces = '       '
-    return seven_spaces.colorize(:background => :white) unless char
-  
-    char == 'R' ? seven_spaces.colorize(:background => :red) : seven_spaces.colorize(:background => :light_yellow)
+    return seven_spaces.colorize(background: :white) unless char
+
+    char == 'R' ? seven_spaces.colorize(background: :red) : seven_spaces.colorize(background: :light_yellow)
   end
 
   def low_half(char)
     seven_scores = '_______'
-    return seven_scores.colorize(:color => :black, :background => :white) unless char
+    return seven_scores.colorize(color: :black, background: :white) unless char
 
-    char == 'R' ? seven_scores.colorize(:color => :black, :background => :red) : seven_scores.colorize(:color => :black, :background => :light_yellow)
+    if char == 'R'
+      seven_scores.colorize(color: :black,
+                            background: :red)
+    else
+      seven_scores.colorize(
+        color: :black, background: :light_yellow
+      )
+    end
   end
 
   def display_board
-    v = "|".colorize(:color => :black, :background => :white)
+    v = '|'.colorize(color: :black, background: :white)
     numbers = '    1       2       3       4       5       6       7'
     string = ''
-      [5, 4, 3, 2, 1, 0].each do |i|
-        [0, 1, 2, 3, 4, 5, 6].each do |j|
-          string += (v + top_half(cells_array.dig(j, i)))
-        end
-        string += (v + "\n")
-        [0, 1, 2, 3, 4, 5, 6].each do |j|
-          string += (v + low_half(cells_array.dig(j, i)))
-        end
-        string += (v + "\n")
+    [5, 4, 3, 2, 1, 0].each do |i|
+      [0, 1, 2, 3, 4, 5, 6].each do |j|
+        string += (v + top_half(cells_array.dig(j, i)))
       end
+      string += "#{v}\n"
+      [0, 1, 2, 3, 4, 5, 6].each do |j|
+        string += (v + low_half(cells_array.dig(j, i)))
+      end
+      string += "#{v}\n"
+    end
     puts string + numbers
   end
 
@@ -62,18 +67,22 @@ class Board
     return true if horizontal_connect_four?(array, row, disc)
     return true if north_east_connect_four?(array, column, row, disc)
     return true if north_west_connect_four?(array, column, row, disc)
+
     false
   end
 
   def vertical_connect_four?(array, column, row, disc)
-    return true if array.dig(column, row - 1) == disc && array.dig(column, row - 2) == disc && array.dig(column, row - 3) == disc
+    return true if array.dig(column,
+                             row - 1) == disc && array.dig(column,
+                                                           row - 2) == disc && array.dig(column, row - 3) == disc
+
     false
   end
 
   def horizontal_connect_four?(array, row, disc)
-    for i in [0, 1, 2, 3] do
+    [0, 1, 2, 3].each do |i|
       four_in_a_row = true
-      for j in [i, i + 1, i + 2, i + 3] do
+      [i, i + 1, i + 2, i + 3].each do |j|
         four_in_a_row = false unless array.dig(j, row) == disc
       end
       return true if four_in_a_row
@@ -83,9 +92,9 @@ class Board
 
   def north_east_connect_four?(array, column, row, disc)
     diff = column - row
-    for i in [0, 1, 2, 3] do
+    [0, 1, 2, 3].each do |i|
       four_in_a_north_east_diagonal = true
-      for j in [i, i + 1, i + 2, i + 3] do
+      [i, i + 1, i + 2, i + 3].each do |j|
         four_in_a_north_east_diagonal = false unless array.dig(j, j - diff) == disc
       end
       return true if four_in_a_north_east_diagonal
@@ -95,9 +104,9 @@ class Board
 
   def north_west_connect_four?(array, column, row, disc)
     sum = column + row
-    for i in [0, 1, 2, 3] do
+    [0, 1, 2, 3].each do |i|
       four_in_a_north_west_diagonal = true
-      for j in [i, i + 1, i + 2, i + 3] do
+      [i, i + 1, i + 2, i + 3].each do |j|
         four_in_a_north_west_diagonal = false unless array.dig(j, sum - j) == disc
       end
       return true if four_in_a_north_west_diagonal
@@ -108,9 +117,9 @@ class Board
   def check_if_game_drawn?(array, row)
     return false unless row == 5
 
-    return true unless [array.dig(0, 5), array.dig(1, 5), array.dig(2, 5), array.dig(3, 5), array.dig(4, 5), array.dig(5, 5), array.dig(6, 5)].include?(nil)
+    return true unless [array.dig(0, 5), array.dig(1, 5), array.dig(2, 5), array.dig(3, 5), array.dig(4, 5),
+                        array.dig(5, 5), array.dig(6, 5)].include?(nil)
 
     false
   end
 end
-
